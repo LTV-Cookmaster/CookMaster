7ad38e004a0ac77416170bf05320285e3856543f
Lukas Ambry <lambry@wedigital.garden>
2023-05-01 13:35:35 +0200

    feat:database migrations

---
+++ CookMaster/database/migrations/2023_04_30_162107_create_invoices_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+
+    // Invoices = factures
+
+    public function up(): void
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignUuid('user_id')->constrained('users');
+            $table->foreignUuid('room_id')->constrained('rooms');
+            $table->foreignUuid('contractor_id')->constrained('contractors');
+            $table->string('description');
+            $table->string('price');
+            $table->timestamps();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('invoices');
+    }
+};
