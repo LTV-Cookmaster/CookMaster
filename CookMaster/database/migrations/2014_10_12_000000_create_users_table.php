f98b10174e63b1ea0fa3f9a956b86373e49210aa
Lukas Ambry <lambry@wedigital.garden>
2023-05-20 18:44:40 +0200

    feat/adding boolean is_admin in users migration

--- CookMaster/database/migrations/2014_10_12_000000_create_users_table.php
+++ CookMaster/database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,6 +23,7 @@ return new class extends Migration
             $table->string('phone');
             $table->string('password');
             $table->string('referral_code')->nullable();
+            $table->boolean('is_admin')->default(false);
             $table->rememberToken();
             $table->timestamps();
         });
