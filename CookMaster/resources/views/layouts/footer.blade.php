1d30511d5bdd61e7aa076ce21acf6e5ac9e6b04b
LukasAmbry <lambry@wedigital.garden>
2023-05-20 18:11:47 +0200

    feat: navbar, footer and home page

---
+++ CookMaster/resources/views/layouts/footer.blade.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <title>@yield('title')</title>
+</head>
+<div class="container-fluid mt-3">
+    <footer class="d-flex flex-wrap justify-content-between align-items-center">
+        <p class="col-md-4 mb-0 text-muted">Cookmaster ©</p>
+
+        <a href="#"
+           class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+            <img src="{{asset('logo.png')}}" alt="cookmaster" height="32" width="32">
+        </a>
+
+        <ul class="nav col-md-4 justify-content-end">
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">About us</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Privacy policy</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-muted disabled">cookmaster@gmail.com</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2"><i class="fa-brands fa-facebook" style="color: #1C6513"></i></a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2"><i class="fa-brands fa-twitter" style="color: #1C6513"></i></a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2"><i class="fa-brands fa-instagram" style="color: #1C6513"></i></a></li>
+        </ul>
+    </footer>
+</div>
+
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.2.0/mdb.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+</html>
