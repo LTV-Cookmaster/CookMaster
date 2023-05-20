bfe2b056a3f855eba0217e0684267c0d3cd01e5a
LukasAmbry <lambry@wedigital.garden>
2023-05-20 19:15:53 +0200

    Feat: adding admin account in db and refacto seeders

--- CookMaster/database/seeders/UserSeeder.php
+++ CookMaster/database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -13,6 +14,17 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory()->count(10)->create();
+        User::factory()->create([
+            'name'=> 'Admin',
+            'email' => 'admin@test.com',
+            'address' => '1 rue de la Paix',
+            'postal_code' => '75001',
+            'city' => 'Paris',
+            'country' => 'France',
+            'phone' => '0123456789',
+            'password' => Hash::make('admin'),
+            'is_admin' => true,
+        ]);
+        User::factory(10)->create();
     }
 }
