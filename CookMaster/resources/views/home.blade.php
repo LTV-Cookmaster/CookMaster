ab29ac581a90f3dc090112a0b60d2c91ed0716fd
LukasAmbry <lambry@wedigital.garden>
2023-05-24 17:40:47 +0200

    first try on home page after logging and subbing

--- CookMaster/resources/views/home.blade.php
+++ CookMaster/resources/views/home.blade.php
@@ -1,23 +1,106 @@
-@extends('layouts.navbar')
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <link rel="preload" href="{{ asset('delivery.jpg') }}" as="image">
+    <link rel="preload" href="{{ asset('menu.jpg') }}" as="image">
+    <link rel="preload" href="{{ asset('profcook.jpg') }}" as="image">
+    <link rel="preload" href="{{ asset('workshop.jpg') }}" as="image">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+    <title>@yield('title')</title>
 
-                    {{ __('You are logged in!') }}
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+</head>
+
+<body>
+@include('layouts.navbar')
+
+<div class="container-fluid">
+    <div class="container">
+        <h2 class="text-center">Your next workshops</h2>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card mt-4">
+                    <div class="card-body">
+                        <a href="#">
+                        <img src="{{ asset('logo.png') }}" alt="hello" height="200" width="200">
+                        </a>
+                    </div>
+                    <div class="card-body">
+                        <span>hello</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card mt-4">
+                    <div class="card-header">
+                        <a href="#">
+                            <img src="{{ asset('logo.png') }}" alt="hello" height="200" width="200">
+                        </a>
+                    </div>
+                    <div class="card-body">
+                        <span>hello2</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card mt-4">
+                    <div class="card-header">
+                        <a href="#">
+                            <img src="{{ asset('logo.png') }}" alt="hello" height="200" width="200">
+                        </a>
+                    </div>
+                    <div class="card-body">
+                        <span>hello3</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <h2 class="text-center">Your next events</h2>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card mt-4">
+                    <div class="card-header">
+                        <a href="#">
+                            <img src="{{ asset('logo.png') }}" alt="hello" height="200" width="200">
+                        </a>
+                    </div>
+                    <div class="card-body">
+                        <span>hello</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card mt-4">
+                    <div class="card-header">
+                        <a href="#">
+                            <img src="{{ asset('logo.png') }}" alt="hello" height="200" width="200">
+                        </a>
+                    </div>
+                    <div class="card-body">
+                        <span>hello2</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card mt-4">
+                    <div class="card-header">
+                        <a href="#">
+                            <img src="{{ asset('logo.png') }}" alt="hello" height="200" width="200">
+                        </a>
+                    </div>
+                    <div class="card-body">
+                        <span>hello3</span>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
-@endsection
+@include('layouts.footer')
+</body>
+</html>
