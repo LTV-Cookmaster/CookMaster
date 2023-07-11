cb5c5b6e036fe71414e274f8d886759fc09e86d4
LukasAmbry <lambry@wedigital.garden>
2023-07-11 16:00:04 +0200

    configuring kernel

--- app/Http/Middleware/LanguageSwitch.php
+++ app/Http/Middleware/LanguageSwitch.php
@@ -4,6 +4,8 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Session;
 use Symfony\Component\HttpFoundation\Response;
 
 class LanguageSwitch
@@ -15,8 +17,6 @@ class LanguageSwitch
      */
     public function handle(Request $request, Closure $next): Response
     {
-        //                $locale = Session::get('locale', config('app.locale'));
-        //                App::setLocale($locale);
         if ($request->has('lang')) {
             $lang = $request->input('lang');
             if (in_array($lang, ['en', 'fr'])) {
