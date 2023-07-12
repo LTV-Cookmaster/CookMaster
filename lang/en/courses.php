780fada5469877c0b96dcbb2a02a3cd07535f76d
Lukas Ambry <lambry@wedigital.garden>
2023-07-12 14:00:28 +0200

    adding courses trad

---
+++ lang/en/courses.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'title' => 'Courses',
+    'available_formations' => 'Your available formations',
+    'graduating_formations' => 'Your graduating formations',
+    'name' => 'Name',
+    'type' => 'Type',
+    'price' => 'Price',
+    'actions' => 'Actions',
+    'configured_formation' => 'Configured formation',
+    'to_configure_formation' => 'To configure formation',
+];
