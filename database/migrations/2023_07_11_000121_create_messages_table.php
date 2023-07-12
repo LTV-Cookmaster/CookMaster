32ee77f45636096ffae2dd491ef6655a5ef829c0
Lukas Ambry <lambry@wedigital.garden>
2023-07-12 14:34:02 +0200

    finally success on me

--- database/migrations/2023_07_11_000121_create_messages_table.php
+++ database/migrations/2023_07_11_000121_create_messages_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('messages', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->integer('user_id')->unsigned();
+            $table->uuid('user_id');
             $table->text('message');
         });
 
